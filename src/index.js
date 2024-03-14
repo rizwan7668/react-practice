8d0d64d3d3ea6e09bebd11adc5ee773f98deda6d
Rizwan <rizwan7668494921@gmail.com>
2024-03-14 11:31:58 +0530

    file-modification

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap"
 import "./index.css"
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
-import Sidebar from "./sidebar/Sidebar";
-import Main from "./main/Main";
+import App from "./App"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
-      <Header />
-      <div className="home">
-        <Sidebar />
-        <Main />
-      </div>
-      <Footer />
-    </>
+    <App />
   </React.StrictMode>
 );
